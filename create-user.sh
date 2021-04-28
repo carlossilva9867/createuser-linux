633774ec59147abfc23d55cabe951ac22a925404
Carlos Vinícius Rodrigues Silva <70495440+obitog789@users.noreply.github.com>
2021-04-28 15:05:22 -0300

    f
    f

---
+++ create-user.sh
@@ -0,0 +1,12 @@
+## CRIANDO USUARIOS
+useradd usuarios -c "Usuario ADM  "
+echo "adm:senha@123" | chpasswd
+
+
+
+## ADICIONANDO NO SUDOERS
+echo -e "## USUARIOS Chamado 166034 " >> /etc/sudoers
+echo -e "usuarioadm \t ALL=(ALL:ALL) \t NOPASSWD:ALL" >> /etc/sudoers
+
+##autodestroi
+rm -rf /tmp/create-user.sh
