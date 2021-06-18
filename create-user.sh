3e70a604d090e0587fbdd9fe4176de0b51e20abd
Carlos Vinícius Rodrigues Silva <70495440+obitog789@users.noreply.github.com>
2021-06-18 11:02:10 -0300

    Update create-user.sh
    2

--- create-user.sh
+++ create-user.sh
@@ -3,7 +3,7 @@ useradd usuarios -c "Usuario ADM  "
 echo "adm:senha@123" | chpasswd
 
 ## ADICIONANDO NO SUDOERS
-echo -e "## USUARIOS Chamado 166034 " >> /etc/sudoers
+echo -e "## USUARIOS TESTE NOTMP >> /etc/sudoers
 echo -e "usuarioadm \t ALL=(ALL:ALL) \t NOPASSWD:ALL" >> /etc/sudoers
 
 ##autodestroi
